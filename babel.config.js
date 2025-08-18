1b166d02ce1fb3806b431b0420523f07418558f2
Gabi27 <sidaroman_gabriel@yahoo.com>
2025-08-19 00:59:51 +0300

    Added module-resolver plugin and mirrored aliases from tsconfig.json

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-worklets/plugin'], //worklets (reanimated) plugin must be last in array
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@state': './src/state',
+          '@utils': './src/utils',
+        },
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+      },
+    ],
+    'react-native-worklets/plugin', //worklets (reanimated) plugin must be last in array
+  ],
 };
