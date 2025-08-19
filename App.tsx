a8f11fe12250bd68fafd35c2c73334be2efd1f6d
Gabi27 <sidaroman_gabriel@yahoo.com>
2025-08-19 03:28:41 +0300

    Navigation functionality tests

--- App.tsx
+++ App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import FirebaseEmailAuth from './testing(delete when finished)/FirebaseEmailAuth.tsx';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from '@navigation/AppNavigator';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.root} edges={['top', 'bottom']}>
-        <FirebaseEmailAuth />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  root: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-});
