57122defca0e32fba6e634d4cfad2f5f86262541
Gabi27 <sidaroman_gabriel@yahoo.com>
2025-08-23 23:35:01 +0300

    Removed duplicate NavigationContainer from App.tsx (it's now used in AppNavigator.tsx)

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from '@navigation/AppNavigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <SafeAreaProvider>
       <AppNavigator />
-    </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
