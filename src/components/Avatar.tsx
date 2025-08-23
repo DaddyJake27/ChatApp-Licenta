48ab2235508576c902e02f0521fe97ccead77aa9
Gabi27 <sidaroman_gabriel@yahoo.com>
2025-08-23 23:19:57 +0300

    Added initial code

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+export default function Avatar() {
+  return <View style={s.a} />;
+}
+const s = StyleSheet.create({
+  a: { width: 36, height: 36, borderRadius: 18, backgroundColor: '#ddd' },
+});
