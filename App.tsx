c32af4fd6fbdd2fd2db11ee2cf3b5094da909aa7
Gabi27 <sidaroman_gabriel@yahoo.com>
2025-08-24 03:49:07 +0300

    Added effect to hide native splash once React is mounted

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import BootSplash from 'react-native-bootsplash';
 import AppNavigator from '@navigation/AppNavigator';
 
 export default function App() {
+  useEffect(() => {
+    // hides the native (static) splash once React is mounted
+    BootSplash.hide({ fade: true });
+  }, []);
+
   return (
     <SafeAreaProvider>
       <AppNavigator />
