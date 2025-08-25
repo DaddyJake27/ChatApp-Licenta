e8bba198879c49954299e52ccd301268d085ed4c
Gabi27 <sidaroman_gabriel@yahoo.com>
2025-08-25 14:14:33 +0300

    Solved deprecated Firebase namespace API warning

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react';
-import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import {
+  getAuth,
+  onAuthStateChanged,
+  FirebaseAuthTypes,
+} from '@react-native-firebase/auth';
 
 export default function useAuth() {
   const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
   const [initializing, setInitializing] = useState(true);
 
   useEffect(() => {
-    const unsub = auth().onAuthStateChanged(u => {
+    const auth = getAuth();
+    const unsub = onAuthStateChanged(auth, u => {
       setUser(u);
       if (initializing) setInitializing(false);
     });
